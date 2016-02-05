7dae6d4490a4394ef6a373e38cdcdd0d2c457c5d
quantumtom <cornyn@gmail.com>
2016-02-05 15:16:02 -0800

    Messing around with a new custom stack.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 
+// File: js/main.js
+
+// 'jquery' returns the jQuery object into '$'
+//
+// 'bootstrap' does not return an object. Must appear at the end
+
+require(['jquery', 'bootstrap'], function($){
+
+  // DOM ready
+  $(function(){
+
+    // Twitter Bootstrap 3 carousel plugin
+    $("#element").carousel();
+  });
+});
