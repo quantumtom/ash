c2efea9ae442886f7f52749fdc4e6c372bbfb380
quantumtom <cornyn@gmail.com>
2016-02-06 14:06:39 -0800

    Initial commit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,37 @@
-/**
- * Created by thomascornyn on 2/6/16.
- */
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      revisedFiles: {
+        src: [
+          'app/assets/javascripts/**/*.js',
+          '!app/assets/javascripts/*.js',
+          '!app/assets/javascripts/bootstrap-components/*'
+        ]
+      },
+      options: {
+        reporter: require('jshint-stylish'),
+        jshintrc: '.jshintrc'
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-check-dependencies');
+
+  grunt.registerTask('default', 'Default grunt task.', [
+    'jshint'
+  ]);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('build', ['jshint']);
+
+};
