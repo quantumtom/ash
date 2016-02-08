1190e179c2db014b98382048323b98a042dcc1cb
quantumtom <cornyn@gmail.com>
2016-02-07 21:07:16 -0800

    Remove extra comment.

--- src/js/app.js
+++ src/js/app.js
@@ -10,10 +10,7 @@ requirejs.config({
     // the right side is the path to
     // the jQuery file, relative to baseUrl.
     // Also, the path should NOT include
-    // the '.js' file extension. This example
-    // is using jQuery 1.9.0 located at
-    // js/lib/jquery-1.9.0.js, relative to
-    // the HTML page.
+    // the '.js' file extension.
     bootstrap:      'lib/bootstrap',
     jquery:         'lib/jquery',
     Handlebars:     'lib/handlebars',
