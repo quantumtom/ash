076959cc68e3bb89e3cc09da638f38c16545b7a5
quantumtom <cornyn@gmail.com>
2016-02-08 21:27:38 -0800

    Sheesh. Finally. Got Grunt RequireJS task running to build dist file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,32 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      dist  : {
+        options: {
+          baseUrl: 'src/js',
+          name: 'app',
+          optimize: 'none',
+          out: 'dist/js/requirejs.js',
+          paths: {
+            'templates':  'app/parts',
+            'jquery':     'lib/jquery',
+            'bootstrap':  'lib/bootstrap',
+            'handlebars': 'lib/handlebars',
+            'controllers': 'app/controllers',
+            'data': 'app/data',
+            'models': 'app/models',
+            'parts': 'app/parts',
+            'views': 'app/views',
+            'router': 'app/router'
+          },
+          preserveLicenseComments: false,
+          useStrict: true,
+          wrap: true,
+          wrapShim: true
+        }
+      }
+    },
     jshint: {
       simple: {
         src: [
