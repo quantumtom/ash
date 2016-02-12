844aba5f5765bbda868cd3cdb4770f6c550838b2
quantumtom <cornyn@gmail.com>
2016-02-11 19:20:45 -0800

    Add semicolon.

--- app/app.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.use('/', express.static(__dirname + '/src'));
-app.listen(3000, function() { console.log('listening')});
--- serve.js
+++ serve.js
@@ -4,4 +4,4 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/src'));
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() { console.log('listening')});
+app.listen(app.get('port'), function() { console.log('listening');});
