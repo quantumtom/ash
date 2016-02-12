f72a14cf7630b90bcc50e6f8dc6d30ebcb65324e
quantumtom <cornyn@gmail.com>
2016-02-11 18:20:51 -0800

    Clean up conf.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,50 @@
 module.exports = function (grunt) {
 
-  var conf = {
-    pkg: grunt.file.readJSON('package.json'),
-    requirejs: {
-      dist: {
-        options: {
-          baseUrl: 'src/js',
-          name: 'app',
-          optimize: 'none',
-          out: 'dist/js/app.js',
-          paths: {
-            'templates': 'app/parts',
-            'jquery': 'lib/jquery',
-            'bootstrap': 'lib/bootstrap',
-            'handlebars': 'lib/handlebars',
-            'controllers': 'app/controllers',
-            'data': 'app/data',
-            'models': 'app/models',
-            'parts': 'app/parts',
-            'views': 'app/views',
-            'router': 'app/router'
-          }
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  requirejs: {
+    dist: {
+      options: {
+        baseUrl: 'src/js',
+        name: 'app',
+        optimize: 'none',
+        out: 'dist/js/app.js',
+        paths: {
+          'templates': 'app/parts',
+          'jquery': 'lib/jquery',
+          'bootstrap': 'lib/bootstrap',
+          'handlebars': 'lib/handlebars',
+          'controllers': 'app/controllers',
+          'data': 'app/data',
+          'models': 'app/models',
+          'parts': 'app/parts',
+          'views': 'app/views',
+          'router': 'app/router'
         }
       }
-    },
-    cssmin: {
-      dist  : [{
-        src: ['src/css/*.css', '!src/css/*.min.css'],
-        dest: 'dist/css/app.css'
-      }]
-    },
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'src/js/app/**/*.js',
-        'src/js/app.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
     }
-  };
-
-  grunt.initConfig(conf);
+  },
+  cssmin: {
+    dist  : [{
+      src: ['src/css/*.css', '!src/css/*.min.css'],
+      dest: 'dist/css/app.css'
+    }]
+  },
+  jshint: {
+    all: [
+      'Gruntfile.js',
+      'src/js/app/**/*.js',
+      'src/js/app.js'
+    ],
+    options: {
+      jshintrc: '.jshintrc'
+    }
+  },
+  watch: {
+    files: ['<%= jshint.files %>'],
+    tasks: ['jshint']
+  }
+});
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
--- serve.js
+++ serve.js
@@ -3,4 +3,5 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/src'));
-app.listen(3000, function() { console.log('listening')});
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() { console.log('listening')});
