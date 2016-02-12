67841ae3acfc614ed44aea486893a43f18012f61
quantumtom <cornyn@gmail.com>
2016-02-11 21:41:20 -0800

    Change port number.

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
