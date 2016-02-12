ea2a8040ea858c2c8999986d457ab6a90b3f69d0
quantumtom <cornyn@gmail.com>
2016-02-11 18:23:56 -0800

    Reorder main dependencies.

--- src/js/app.js
+++ src/js/app.js
@@ -11,18 +11,18 @@ requirejs.config({
     // the jQuery file, relative to baseUrl.
     // Also, the path should NOT include
     // the '.js' file extension.
-    bootstrap:      'lib/bootstrap',
-    twbs:           'lib/twbs',
-    jquery:         'lib/jquery',
-    Handlebars:     'lib/handlebars',
-    text:           'lib/text',
-    hbar:           'lib/hbars',
     router:         'app/router',
     parts:          'app/parts',
     controllers:    'app/controllers',
     data:           'app/data',
     models:         'app/models',
-    views:          'app/views'
+    views:          'app/views',
+    bootstrap:      'lib/bootstrap',
+    twbs:           'lib/twbs',
+    jquery:         'lib/jquery',
+    Handlebars:     'lib/handlebars',
+    text:           'lib/text',
+    hbar:           'lib/hbars'
   },
   shim: {
     bootstrap: {
