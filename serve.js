f4509be2af35e84b30561a4ecd009e8fe632ec9d
quantumtom <cornyn@gmail.com>
2016-02-11 16:06:01 -0800

    Remove extra serve file.

--- serve.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.use('/', express.static(__dirname + '/src'));
-app.listen(process.env.PORT || 5000, function() { console.log('listening')});
