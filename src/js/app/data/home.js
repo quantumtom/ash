e1f0ebf151b77ccf47e06ebfff8d05693e04e95d
quantumtom <cornyn@gmail.com>
2016-02-18 15:37:53 -0800

    Huge update.

--- src/js/app/data/home.js
+++ src/js/app/data/home.js
@@ -1,6 +1,7 @@
 define(function () {
   return {
     title: 'Home Page',
-    body: 'A Bootstrap demo implemented through Handlebars via RequireJS.'
+    lead: 'A Bootstrap demo implemented through Handlebars via RequireJS.',
+    body: 'This is a modular micro-MVC single-page web app serving Bootstrap components through Handlebars with Require (via ExpressJS). A minimal stack that renders a "living template" of Bootstrap GUI widgets (components) using the HandlebarsJS template system. All thanks to RequireJS.'
   };
 });
