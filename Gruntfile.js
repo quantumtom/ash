ca097d8006690fcb8aa8c38c569874a525bed188
quantumtom <cornyn@gmail.com>
2016-02-19 14:47:25 -0800

    Remove version attribute.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    version: pkg.version,
     clean: ['dist'],
     copy: {
       main: {
