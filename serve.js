f5ec73d195dab32067a380989affd03e72f3dddf
quantumtom <cornyn@gmail.com>
2016-03-13 13:58:33 -0700

    Add GZip compression for Node/ExpressJS.

--- serve.js
+++ serve.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var compress = require('compression');
 
+app.use(compress());
 app.use('/', express.static(__dirname + '/dist'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function() { console.log('listening');});
